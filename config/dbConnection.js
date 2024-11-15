6c617d0e0b1add09454cac4c6f0be8ee204ab4c7
pedrosiufc <pedrosiufc@gmail.com>
2024-11-15 12:05:30 -0300

    Instalando o módulo do MongoDB e estabelecendo conexão

---
+++ config/dbConnection.js
@@ -0,0 +1,23 @@
+//importar o mongodb
+var mongo = require('mongodb');
+
+var connMongoDB = function(){
+  console.log('entrou na conexão com o banco!')
+  var db = new mongo.Db(
+   //nome de banco
+    'got', 
+   // objeto de conexão com o servidor
+    new mongo.ServerClosedEvent( //para minha versão
+       'localhost', //endereço do banco de dados 
+       27017, //porta de conexão
+       {} //configurações opcinais
+    ), 
+      {} //configurações opcionais
+  );
+  return db; //para utilizar dentros dos models
+}
+
+module.exports = function(){
+  return connMongoDB;
+  
+}
