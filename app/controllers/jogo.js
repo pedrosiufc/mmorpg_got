b1c51c67679508b4826e90f3e61691685af928cf
pedrosiufc <pedrosiufc@gmail.com>
2024-12-15 16:34:41 -0300

    Carregando as views de súditos e pergaminhos via Ajax com JQuery e correção da ejs aldeoes

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,8 +1,8 @@
-module.exports.jogo = function (application, req, res) {
+module.exports.jogo = function(application, req, res){
 
-	if (req.session.autorizado !== true) {
-		res.send('Usuário precisa fazer login');
-		return;
+	if(req.session.autorizado !== true){
+			res.send('Usuário precisa fazer login');
+			return;   
 	}
 
 	var usuario = req.session.usuario;
@@ -12,13 +12,18 @@ module.exports.jogo = function (application, req, res) {
 	var JogoDAO = new application.app.models.JogoDAO(connection);
 
 	JogoDAO.iniciaJogo(res, usuario, casa);
-
-
 }
 
-module.exports.sair = function (application, req, res) {
-
-	req.session.destroy(function (err) {
-		res.render("index", { validacao: {} });
+module.exports.sair = function(application, req, res){
+	req.session.destroy(function(err){
+			res.render('index', { validacao: {} });
 	});
+}
+
+module.exports.suditos = function(application, req, res){
+	res.render('aldeoes', { validacao: {} });
+}
+
+module.exports.pergaminhos = function(application, req, res){
+	res.render('pergaminhos', { validacao: {} });
 }
