a067f9e58d2401398bae9aa0d6c77a98077442e4
pedrosiufc <pedrosiufc@gmail.com>
2024-12-21 11:36:55 -0300

    Removendo ações do Banco de Dados

--- app/models/JogoDAO.js
+++ app/models/JogoDAO.js
@@ -1,3 +1,5 @@
+var ObjectID = require('mongodb').ObjectID;
+
 function JogoDAO(connection) {
   this._connection = connection();
 }
@@ -96,6 +98,22 @@ JogoDAO.prototype.getAcoes = function (usuario, res) {
 
         mongoclient.close();
       });
+    });
+  });
+}
+
+JogoDAO.prototype.revogarAcao = function (_id, res){
+  this._connection.open(function (err, mongoclient) {
+    mongoclient.collection("acao", function (err, collection) {
+        collection.remove(
+          {_id : ObjectID(_id)},
+          function(err, result){
+            res.redirect("jogo?msg=D");
+            mongoclient.close();
+          }
+
+        );
+      
     });
   });
 }
